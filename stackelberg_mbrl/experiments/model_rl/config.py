a1e5ce302c2d310f06e574ce53407ff2d508d679
Yanick Zengaffinen <yanick.zengaffinen@gmail.com>
2024-05-20 22:58:19 +0200

    use simple_mdp_2

--- stackelberg_mbrl/experiments/model_rl/config.py
+++ stackelberg_mbrl/experiments/model_rl/config.py
@@ -4,8 +4,8 @@ from stackelberg_mbrl.experiments.experiment_config import ExperimentConfig, Env
 model_rl_config = ExperimentConfig(
     experiment_name="model_rl",
     env_config=EnvConfig(
-        env_true_id="simple_mdp_2_variant_2",
-        env_eval_id="simple_mdp_2_variant_1",
+        env_true_id="simple_mdp_2",
+        env_eval_id="simple_mdp_2_variant_2",
         max_episode_steps=50
     ),
     policy_config=LoadPolicy(
