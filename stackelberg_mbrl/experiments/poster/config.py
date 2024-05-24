eb6d5ad238d50252a7b739779ddc79eb6802d8aa
Nils Cremer <nils.crmr@gmail.com>
2024-05-24 23:36:54 +0200

    Add experiments results

--- stackelberg_mbrl/experiments/poster/config.py
+++ stackelberg_mbrl/experiments/poster/config.py
@@ -2,22 +2,22 @@ from stackelberg_mbrl.experiments.experiment_config import ExperimentConfig, Env
 
 
 poster_config = ExperimentConfig(
-    experiment_name="poster",
+    experiment_name="poster_pal",
     env_config=EnvConfig(
-        env_true_id="simple_mdp_2_endless",
+        env_true_id="simple_mdp_2",
         env_eval_id="simple_mdp_2",
         max_episode_steps=50
     ),
     policy_config=PolicyConfig(
-        pretrain_iterations=20,
-        samples_per_training_iteration=10_000,
+        pretrain_iterations=1,
+        samples_per_training_iteration=250_000,
     ),
     leader_env_config=LeaderEnvConfig(
         env_reward_weight=0.0
     ),
     world_model_config=WorldModelConfig(
         total_training_steps=250_000,
-        model_save_name="simple_mdp_2_endless",
+        model_save_name="simple_mdp_2",
     ),
     seed=14
 )
